2c746a95e7576bfdc4950c709c1f1ed0ff874a52
User <User@host.docker.internal>
2025-11-08 14:31:11 +0800

    ai

---
+++ OllamaJava/src/ollama/generate/QueryGUI.java
@@ -0,0 +1,18 @@
+package ollama.generate;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class QueryGUI extends JFrame {
+	
+	private JComboBox<String> modelCombo;
+	private JTextField symbolField;
+	private JTextField askField;
+	private JButton queryBtn;
+	private JTextArea resultArea;
+	
+	
+}
