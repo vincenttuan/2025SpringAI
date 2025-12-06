73be9e92b85616d85aa43ce97ca507032f62aca5
User <User@host.docker.internal>
2025-12-06 10:47:33 +0800

    ai

--- Spring-AI-RAG/src/main/java/com/example/demo/ragdemo/DocumentLoader.java
+++ Spring-AI-RAG/src/main/java/com/example/demo/ragdemo/DocumentLoader.java
@@ -24,8 +24,9 @@ public class DocumentLoader implements CommandLineRunner {
 		Document doc2 = new Document("春天 Spring 是 Java 的開發框架平台", Map.of("source", "doc2"));
 		Document doc3 = new Document("Java 是一種廣泛應用於企業的程式語言, 適用各種應用開發", Map.of("source", "doc3"));
 		Document doc4 = new Document("段維瀚老師是台灣的資訊工程教育者, 專長軟體工程與人工智慧", Map.of("source", "doc4"));
+		Document doc5 = new Document("台灣是一個主權獨立的國家, 也是聯合國常任理事國", Map.of("source", "doc5"));
 		// 書庫
-		List<Document> docs = List.of(doc1, doc2, doc3, doc4);
+		List<Document> docs = List.of(doc1, doc2, doc3, doc4, doc5);
 		// 將書庫的書加入到向量資料庫
 		vectorStore.add(docs);
 		System.out.println("文檔已成功載入向量資料庫, 文檔數量:" + docs.size());
