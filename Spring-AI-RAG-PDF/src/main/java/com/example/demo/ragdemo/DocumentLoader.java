9fd22e7acba60ea52aaf1f477faec823dd61683c
User <User@host.docker.internal>
2025-12-06 14:26:16 +0800

    ai

--- Spring-AI-RAG-PDF/src/main/java/com/example/demo/ragdemo/DocumentLoader.java
+++ Spring-AI-RAG-PDF/src/main/java/com/example/demo/ragdemo/DocumentLoader.java
@@ -2,10 +2,14 @@ package com.example.demo.ragdemo;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,17 +23,20 @@ public class DocumentLoader implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		// 書本
-		Document doc1 = new Document("台灣是位於東亞的島嶼國家, 首都為台北", Map.of("source", "doc1"));
-		Document doc2 = new Document("春天 Spring 是 Java 的開發框架平台", Map.of("source", "doc2"));
-		Document doc3 = new Document("Java 是一種廣泛應用於企業的程式語言, 適用各種應用開發", Map.of("source", "doc3"));
-		Document doc4 = new Document("段維瀚老師是台灣的資訊工程教育者, 專長軟體工程與人工智慧", Map.of("source", "doc4"));
-		Document doc5 = new Document("台灣是一個主權獨立的國家, 也是聯合國常任理事國", Map.of("source", "doc5"));
+		// 書本(PDF) 放在 src/main/respurces/docs
+		List<Document> pdfDocs = Stream.of(
+					new PagePdfDocumentReader(new ClassPathResource("docs/eat.pdf")).read(),    // List<Document>
+					new PagePdfDocumentReader(new ClassPathResource("docs/java.pdf")).read(),   // List<Document>
+					new PagePdfDocumentReader(new ClassPathResource("docs/spring.pdf")).read(), // List<Document>
+					new PagePdfDocumentReader(new ClassPathResource("docs/taiwan.pdf")).read(), // List<Document>
+					new PagePdfDocumentReader(new ClassPathResource("docs/teacher.pdf")).read() // List<Document>
+				).flatMap(List::stream) // List<List<Document>> 變成 List<Document> -> List<Document> -> List<Document>...
+				.collect(Collectors.toList());
+		
 		// 書庫
-		List<Document> docs = List.of(doc1, doc2, doc3, doc4, doc5);
 		// 將書庫的書加入到向量資料庫
-		vectorStore.add(docs);
-		System.out.println("文檔已成功載入向量資料庫, 文檔數量:" + docs.size());
+		vectorStore.add(pdfDocs);
+		System.out.println("文檔已成功載入向量資料庫, 文檔數量:" + pdfDocs.size());
 		
 	}
 	
