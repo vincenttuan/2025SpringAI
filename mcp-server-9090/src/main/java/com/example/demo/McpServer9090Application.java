82d9347352663800a0d08c0eb57fcd09b1ee5d70
User <User@host.docker.internal>
2025-12-13 11:54:57 +0800

    ai

--- mcp-server-9090/src/main/java/com/example/demo/McpServer9090Application.java
+++ mcp-server-9090/src/main/java/com/example/demo/McpServer9090Application.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.service.ShoppingService;
 
 @SpringBootApplication
 public class McpServer9090Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(McpServer9090Application.class, args);
+	}
+	
+	@Bean
+	ToolCallbackProvider toolCallbackProvider(ShoppingService shoppingService) {
+		return MethodToolCallbackProvider
+				.builder()
+				.toolObjects(shoppingService)
+				.build();
 	}
 
 }
